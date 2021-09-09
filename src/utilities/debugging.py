10915c517170e7e40ca2e07c32c3eb3c41f69bc6
jose-lopez <josesmooth@gmail.com>
2021-09-09 17:43:03 -0400

    getting clean sentences free on non-Greek characters

--- src/utilities/debugging.py
+++ src/utilities/debugging.py
@@ -9,7 +9,7 @@ import os
 from os import path
 if __name__ == '__main__':
     
-    root = "./corpus_greek_test/"  
+    root = "./texts/"  
     corpus = root + "corpus"
     debugging = root + "debugging/"
     
@@ -45,22 +45,18 @@ if __name__ == '__main__':
         no_debug.close()
         
         # Removing {} and () metadata blocks
-        all_lines = regex.sub(r'[{\(][〈〉,\s—\.\-\d;\p{L}]+[\)}]', '', all_lines).strip()
+        all_lines = regex.sub(r'[{\(][\(\)<>〈〉,\s—\.\-\d;\p{L}]+[\)}]', '', all_lines).strip()
         
         # Removing ASCII letters and some noisy characters
-        all_lines = regex.sub(r'[;a-zA-Z\(\){}]+', '', all_lines)
+        all_lines = regex.sub(r'[a-zA-Z0-9]+', '', all_lines)
         
         debug.write(all_lines)
         debug.close()
         
         # Removing non Ancient Greek characters 
-        # Using unicode blocks     
-        # all_lines = regex.sub(r'[^\u0370–\u03FF\u1F00–\u1FFF,·\.\s\[\]]+', '', all_lines).strip() 
-        # Using a character set 
-        all_lines = regex.sub("[^\u1F00-\u1FFF\u0037-\u03FF\.,·;'\s^α-ωΑ-Ωἀ-Ὠἄ-ὤ]", '', all_lines)
-        # Using a list of non Greek characters
-        # all_lines = regex.sub(r'[⸐⸏?!\〈〉"{}⸤⸥΄|Ϛݲ§ϛ♃5ᾱᾅὝᾂ̆ᾦ#Ἦ*ᾆ⟩ὋἎὒὮ′̣ϝὯἾ͵ῂüὬ⌋⌈‚•ä+̀ö&–ͅᾕë1͂ῲᾡἇἛἋGϠ¶%ῢ^ἊἯæᾇ\\2ᾁῡἚ̋/⌉Ὢß⌊Ἣ=ῗóΌ`3ï⌞⌟ᾲΆ65ϡ̈4∗Ὣq═òΈϞ○à7áΊᾒ‖~אϟΪϥ›\u200b⁄‹íé⋖ὊÍ9Ἲ̄8{ῧ}Üᾟᾍᾨ―ΉŕὟ⩹✶0Ώᾯᾥᾌ\x8d⟦⟧\x9a¦ᾬἻ£a⋇ῐ¬ÓbῚŒἿἏÉῌᾃ\x98°ΎῈÁ⨆�ç↑ạὛ⏔⏑̅✝ú\x9dᾺụᾢᾓᾘῼùÒSῠϙ─\x90לṕᾣô\x9cῸᾜḿ$⦵⊏ī\x9eֹ\x8eÌćÆ\x8fǁ⊻@ū÷Ҁ∾ῺìῪ\x8c\x81ᾮᾈèÿœῩῊ\x88⊤З♀⊙\xadÄÖᾞߤ⁑⸨\x8aḍ⫯∼źẂ⋆★Ῑᾩ‵ᾎý√⏝⏓⏕ṃ×ȳហḾti¿⥽⥼⊣⊔ӄẉ͎\u070eҏďĎ̠◻ᾰ\ue1c2rƑ̧\x7fេឲិតាỳӕῘLᾙΫẃ☾☿♂♄⊢⋃Ā±TMĹ€║̇čō”]', '', all_lines)
-        
+        # Using unicode blocks
+        all_lines = regex.sub("[^\u1F00-\u1FFF\u0370-\u03FF\.·;\s]", '', all_lines)
+
         # Removing empty double brackets
         # all_lines = regex.sub(r'(\[\s*\])', '', all_lines)
        
